7863acd9fa505c49d37e8eb01d69e242e205e1d7
kouame231-ops <erickouame.cri@gmail.com>
2026-08-21 19:44:03 +0000

    Ajout du 2e Text (description justifiée) dans ComposableInfoCard

--- app/src/main/java/com/example/quadrantcompose/MainActivity.kt
+++ app/src/main/java/com/example/quadrantcompose/MainActivity.kt
@@ -125,7 +125,7 @@ fun QuadrantScreen(modifier: Modifier = Modifier) {
     }
 }
 
-// Aperçu (Preview) : permet de voir le résultat dans Android Studio
+// Aperçu (Preview): permet de voir le résultat dans Android Studio
 // sans avoir besoin de lancer l'application sur un téléphone émulateur
 @Preview(showBackground = true)
 @Composable
